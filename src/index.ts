7bbf50f40df29f608b59cf1af09e39f524766db2
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2023-12-09 02:38:29 +0100

    seem to work but well, OCR is OCR, and OCR fail sometimes

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,28 @@ import {
   Point,
   Button
 } from "@nut-tree/nut-js";
-process.env.OPENCV4NODEJS_DISABLE_EXTERNAL_MEM_TRACKING = "1";
+//process.env.OPENCV4NODEJS_DISABLE_EXTERNAL_MEM_TRACKING = "1";
 import * as cv from '@u4/opencv4nodejs';
 
 import fs from "fs";
-import { createWorker } from "tesseract.js";
+import { Worker, createWorker } from "tesseract.js";
 
 
-const processScreenshot = (path: string): boolean => {
+const processScreenshot = (path: string): Array<Buffer> => {
   const img = cv.imread(path);
   const grayImg = img.cvtColor(cv.COLOR_BGR2GRAY);
   const blackAndWhite = grayImg.threshold(0, 255, cv.THRESH_OTSU + cv.THRESH_BINARY);
+
   var cnts = blackAndWhite.findContours(cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+
+  for (let i = 0; i < cnts.length; i++) {
+    const cnt = cnts[i];
+    const rect = cnt.boundingRect();
+    if (rect.width < 10 || rect.height < 10) {
+      cnts.splice(i, 1);
+      i--;
+    }
+  }
 
   cnts = cnts.sort((cnt1, cnt2) => {
     const rect1 = cnt1.boundingRect();
@@ -28,12 +38,13 @@ const processScreenshot = (path: string): boolean => {
 
   const canvasSize = 74; // Set the desired canvas size
   const spaceTopBottom = 10; // Set the desired space at the top and bottom
+  const buffArray: Array<Buffer> = [];
   for (let i = 0; i < cnts.length; i++) {
     const cnt = cnts[i];
     const rect = cnt.boundingRect();
     const cropped = blackAndWhite.getRegion(rect);
 
-    // Create a black canvas with space at the top and bottom
+    // Create a new canvas for each iteration
     const canvas = cv.Mat.zeros(canvasSize + 2 * spaceTopBottom, canvasSize, cv.CV_8UC1);
 
     // Calculate the position to paste the letter in the center with space at the top and bottom
@@ -46,11 +57,71 @@ const processScreenshot = (path: string): boolean => {
     // Copy the letter to the center of the canvas with space at the top and bottom
     cropped.copyTo(roi);
 
-    cv.imwrite(`./cropped/cropped${i}.png`, canvas);
+    buffArray.push(cv.imencode('.png', canvas));
 
+    //cv.imwrite(`./cropped/cropped${i}.png`, canvas);
+  }
+  return buffArray;
+};
+
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const checkAlignment = async (worker: Worker) => {
+  if (getPixelColor(741, 724) !== "e6e8f4") {
+    console.log("Screen no longer aligned");
+    await worker.terminate();
+    process.exit(0);
+  }
+};
+
+const waitForStart = async (x: number, y: number) => {
+  while (getPixelColor(x, y) === "ffffff") {
+    await sleep(10);
+  }
+};
+
+const readScreen = async (x: number, y: number, width: number, height: number) => {
+  fs.readdirSync("./cropped").forEach((file) => {
+    fs.unlinkSync(`./cropped/${file}`);
+  });
+
+  while (getPixelColor(898, 571) === "ffd154") {
+    await sleep(100);
+  }
+  const region = new Region(x, y, width, height);
+  await nutScreen.captureRegion(`./nbr.png`, region);
+};
+
+const recognizeNumber = async (worker: Worker) => {
+
+  const buffArray = processScreenshot(`./nbr.png`);
+  let str = "";
+  for (let i = 0; i < buffArray.length; i++) {
+    const ret = await worker.recognize(buffArray[i]);
+    str = str.concat(ret.data.text.replace('\n', ''));
   }
 
-  return true;
+  return parseInt(str);
+};
+
+const typeAndSubmit = async (number: number) => {
+  while (getPixelColor(892, 512) !== "ffd154") {
+    await sleep(10);
+  }
+  await keyboard.type(number.toString());
+  const point = new Point(892, 540);
+  await mouse.setPosition(point);
+  await mouse.click(Button.LEFT);
+};
+
+const goToNext = async () => {
+  while (getPixelColor(898, 571) !== "ffd154") {
+    await sleep(10);
+  }
+  const point = new Point(898, 571);
+  await mouse.setPosition(point);
+  await mouse.click(Button.LEFT);
 };
 
 (async () => {
@@ -58,77 +129,66 @@ const processScreenshot = (path: string): boolean => {
   setKeyboardDelay(0);
   keyboard.config.autoDelayMs = 0;
   const x = 460;
-  const y = 365;
+  const y = 335;
   const width = 1440 - x;
-  const height = 485 - y;
+  const height = 575 - y;
   var number = 0;
   var written = false;
   var screenRead = false;
 
   const worker = await createWorker("digits");
+  await worker.setParameters({ tessedit_char_whitelist: '0123456789' });
 
-  //902, 567 FFD154 // START
-  //460, 385 // TOP LEFT
-  //1440, 485 //BOT RIGHT
-  //892, 512 FFD154 // SUBMIT
-  //898, 571 FFD154 //NEXT
+  fs.readdirSync("./cropped").forEach((file) => {
+    fs.unlinkSync(`./cropped/${file}`);
+  });
 
-  console.log("Ready to start")
+  console.log("Ready to start");
+  await waitForStart(701, 424);
 
   while (true) {
-    if (getPixelColor(741, 724) !== "e6e8f4") {
-      console.log("Screen no longer alligned");
-      await worker.terminate();
-      process.exit(0);
-    }
-    while (getPixelColor(701, 424) === "ffffff") {
-      await new Promise((resolve) => setTimeout(resolve, 10));
-    }
+    await checkAlignment(worker);
+
     if (!screenRead) {
-      console.log("reading screen")
-      while (getPixelColor(898, 571) === "ffd154") {
-        console.log("Waiting for screen");
-        await new Promise((resolve) => setTimeout(resolve, 100));
-      }
-      console.log("reading screen past while")
-      const region = new Region(x, y, width, height);
-      await nutScreen.captureRegion(`./nbr.png`, region);
+      console.log("Before Reading screen")
+      await readScreen(x, y, width, height);
       screenRead = true;
     } else if (number === 0 && screenRead) {
-      await worker.setParameters({tessedit_char_whitelist: '0123456789'});
-      processScreenshot(`./nbr.png`);
-      var str: string = "";
-      var nbr = fs.readdirSync("./cropped").length;
-      for (let i = 0; i < nbr; i++) {
-        const ret = await worker.recognize(`./cropped/cropped${i}.png`);
-        str = str.concat(ret.data.text.replace('\n', ''));
-        fs.unlinkSync(`./cropped/cropped${i}.png`);
-      }
-
-      number = parseInt(str);
-      console.log(number)
+      console.log("Before recognizeNumber");
+      number = await recognizeNumber(worker);
+      console.log(number);
     } else if (number > 0 && !written) {
-      while (getPixelColor(892, 512) !== "ffd154") {
-        await new Promise((resolve) => setTimeout(resolve, 10));
-      }
-      await keyboard.type(number.toString());
-      const point = new Point(892, 540);
-      await mouse.setPosition(point);
-      await mouse.click(Button.LEFT);
+      console.log("Before typeAndSubmit");
+      await typeAndSubmit(number);
       written = true;
     } else if (written) {
-      while (getPixelColor(898, 571) !== "ffd154") {
-        await new Promise((resolve) => setTimeout(resolve, 10));
-      }
-      console.log("Next")
-      const point = new Point(898, 571);
-      await mouse.setPosition(point);
-      await mouse.click(Button.LEFT);
+      console.log("Before goToNext");
+      await goToNext();
       number = 0;
       screenRead = false;
       written = false;
     }
 
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    await sleep(100);
   }
 })();
+
+
+
+
+
+
+
+// (async () => {
+//     const worker = await createWorker("digits");
+//     await worker.setParameters({ tessedit_char_whitelist: '0123456789' });
+
+//     const test = await recognizeNumber(worker);
+//     console.log(test)
+
+//   const worker2 = await createWorker("eng");
+//   await worker.setParameters({ tessedit_char_whitelist: '0123456789' });
+
+//   const test2 = await recognizeNumber(worker);
+//   console.log(test2)
+// })();
